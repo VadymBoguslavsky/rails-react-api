471ba4514a10c95bc8a944306de57f9ea5def047
VadymBoguslavsky <axixe92@gmail.com>
2017-07-19 11:36:18 +0300

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "rails5-react-redux-todo_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "rails_api-react_client_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
@@ -75,4 +75,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_controller.allow_forgery_protection = false
 end
